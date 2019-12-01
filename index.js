2c516c6958287623a8d4b622cda1569f3b990636
Osamah Bukraa <osamah.bukraa@persgroep.be>
2019-12-01 14:06:14 +0000

    Get live feed and filter kerbspace data based on availability

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const fs = require('fs');
 
 app.use(cors());
 
-let freeparkingspots = [];
+OBJECT_IDS = {
+  TEMPLEWOOD_GARDEN: 'ce9a206e-1106-4931-b833-32e779524429'
+}
+
+let freeparkingspots = {
+  [OBJECT_IDS.TEMPLEWOOD_GARDEN]: []
+};
+let livefeed = {
+  [OBJECT_IDS.TEMPLEWOOD_GARDEN]: './livefeed_templewood_garden.jpg'
+};
 
 app.get('/', async function (req, res) {
     const response = await fetch(
@@ -17,43 +26,60 @@ app.get('/', async function (req, res) {
             }
         }
     );
-    const curbLR = await response.json();
+    const kerbData = await response.json();
 
-    res.send(curbLR);
+    kerbData.features = kerbData.features.map(feature => {
+      if (freeparkingspots[feature.properties.location.objectId] && freeparkingspots[feature.properties.location.objectId].length) {
+        return feature
+      }
+      return null;
+    }).filter(feature => feature !== null);
+
+    res.send(kerbData);
 });
 
 app.get('/livefeed_templewood_garden', async function (req, res) {
-    let imageFile = fs.readFileSync('./livefeed_templewood_garden.jpg');
-    let encodedImg = Buffer.from(imageFile).toString('base64');
-
-    const response = await fetch('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyA8yFjZC5B556_Pkxjz2ltFDKTGxytns4M', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: `{
-            "requests": [
-              {
-                "features": [
-                  {
-                    "maxResults": 50,
-                    "type": "DOCUMENT_TEXT_DETECTION"
-                  }
-                ],
-                "image": {
-                  "content": "${encodedImg}"
-                }
-              }
-            ]
-          }`
-    });
-
-    const imgData = await response.json();
-
-    const freeparking = imgData.responses[0].textAnnotations.filter(annotation => annotation.description.length <= 6 && annotation.description.match(/kerbie|kerble|Kerbie|Kerble|Kerb/));
-
-    res.send(freeparking);
+  const parkingSpotsFound = await updateLiveFeedData(OBJECT_IDS.TEMPLEWOOD_GARDEN)
+  res.send(parkingSpotsFound);
 });
+
+const updateLiveFeedData = async (objectId) => {
+  let imageFile = fs.readFileSync(livefeed[objectId]);
+  let encodedImg = Buffer.from(imageFile).toString('base64');
+
+  const response = await fetch('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyA8yFjZC5B556_Pkxjz2ltFDKTGxytns4M', {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      },
+      body: `{
+          "requests": [
+            {
+              "features": [
+                {
+                  "maxResults": 50,
+                  "type": "DOCUMENT_TEXT_DETECTION"
+                }
+              ],
+              "image": {
+                "content": "${encodedImg}"
+              }
+            }
+          ]
+        }`
+  });
+
+  const imgData = await response.json();
+
+  const freeparking = imgData.responses[0].textAnnotations.filter(annotation => annotation.description.length <= 6 && annotation.description.match(/kerbie|kerble|Kerbie|Kerble|Kerb/));
+
+  freeparkingspots[objectId] = freeparking;
+  return `Found ${freeparking.length} free parking spots in ${objectId}`;
+};
+
+setInterval(async () => {
+  console.log(await updateLiveFeedData(OBJECT_IDS.TEMPLEWOOD_GARDEN));
+}, 2000);
 
 app.listen(3000)
