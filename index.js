ed3d53dcf74e798a014f47982f79f53b852bafd1
Osamah Bukraa <osamah.bukraa@persgroep.be>
2019-12-01 15:03:26 +0000

    Handle error correctly

--- index.js
+++ index.js
@@ -47,44 +47,49 @@ const updateLiveFeedData = async (objectId) => {
   let imageFile = fs.readFileSync(livefeed[objectId]);
   let encodedImg = Buffer.from(imageFile).toString('base64');
 
-  const response = await fetch('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyA8yFjZC5B556_Pkxjz2ltFDKTGxytns4M', {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-      },
-      body: `{
-          "requests": [
-            {
-              "features": [
-                {
-                  "maxResults": 50,
-                  "type": "DOCUMENT_TEXT_DETECTION"
+  try {
+    const response = await fetch('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyA8yFjZC5B556_Pkxjz2ltFDKTGxytns4M', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: `{
+            "requests": [
+              {
+                "features": [
+                  {
+                    "maxResults": 50,
+                    "type": "DOCUMENT_TEXT_DETECTION"
+                  }
+                ],
+                "image": {
+                  "content": "${encodedImg}"
                 }
-              ],
-              "image": {
-                "content": "${encodedImg}"
               }
-            }
-          ]
-        }`
-  });
+            ]
+          }`
+    });
 
-  const imgData = await response.json();
-  console.log(imgData);
+    const imgData = await response.json();
+    // console.log(imgData);
 
-  if (imgData.responses) {
-    const freeparking = imgData.responses[0].textAnnotations.filter(annotation => annotation.description.length <= 6 && annotation.description.match(/kerbie|kerble|Kerbie|Kerble|Kerb/));
+    if (imgData.responses && imgData.responses.length && imgData.responses[0] !== {} && imgData.responses[0].textAnnotations) {
+      const freeparking = imgData.responses[0].textAnnotations.filter(annotation => annotation.description.length <= 6 && annotation.description.match(/kerbie|kerble|Kerbie|Kerble|Kerb/));
 
-    freeparkingspots[objectId] = freeparking;
-    return `Found ${freeparking.length} free parking spots in ${objectId}`;
+      freeparkingspots[objectId] = freeparking;
+      return `Found ${freeparking.length} free parking spots in ${objectId}`;
+    }
+  } catch (error) {
+    console.log(error);
   }
 
-  return `--`;
+  return `Found 0 free parking spots in ${objectId}`;
+
 };
 
-// setInterval(async () => {
-//   console.log(await updateLiveFeedData(OBJECT_IDS.TEMPLEWOOD_GARDEN));
-// }, 2000);
+setInterval(async () => {
+  console.log(await updateLiveFeedData(OBJECT_IDS.TEMPLEWOOD_GARDEN));
+}, 1000);
 
 app.listen(3000)
